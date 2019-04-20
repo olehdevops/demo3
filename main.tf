76476ae1305a244bf11ea3af31c55911af5edf03
olehdevops <swatolegntu@gmail.com>
2019-04-20 19:39:46 +0300

    add tf to Jenkinsfile

--- main.tf
+++ main.tf
@@ -5,18 +5,18 @@ provider "google" {
   project = "careful-time-232710"
 }
 
-//resource "google_storage_bucket" "bucket" {
-//  name = "superdemo3"
-//  location = "EU"
-//
-//}
+resource "google_storage_bucket" "bucket" {
+  name = "superdemo3"
+  location = "EU"
+
+}
 
 resource "google_storage_bucket_object" "saapp" {
   name = "app.zip"
-  bucket = {
-    url = "gs://superdemo3"
-  }
-//  "${google_storage_bucket.bucket.name}"
+//  bucket = {
+//    url = "gs://superdemo3"
+//  }
+  bucket = "${google_storage_bucket.bucket.name}"
   source = "./app.zip"
 
 }
@@ -25,10 +25,10 @@ resource "google_cloudfunctions_function" "get-data" {
   name                  = "get-data"
   description           = "My weather"
   available_memory_mb   = 256
-  source_archive_bucket = {
-    url = "gs://superdemo3"
-  }
-//  "${google_storage_bucket.bucket.name}"
+//  source_archive_bucket = {
+//    url = "gs://superdemo3"
+//  }
+  source_archive_bucket =  "${google_storage_bucket.bucket.name}"
   source_archive_object = "${google_storage_bucket_object.saapp.name}"
   trigger_http          = true
   timeout               = 60
